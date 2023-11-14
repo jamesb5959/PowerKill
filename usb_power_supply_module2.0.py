51f9c786b79d1018a028205af8fa50b7b3769485
Bradley James David Beltran <30552360+jamesb5959@users.noreply.github.com>
2023-11-14 09:58:05 -0700

    Update usb_power_supply_module2.0.py

--- usb_power_supply_module2.0.py
+++ usb_power_supply_module2.0.py
@@ -1,38 +1,64 @@
+import usb.core
+import usb.util
+
 class CustomUSBPowerSupply:
-    @staticmethod
-    def get_max_voltage():
-        # Implement logic to get the maximum voltage
-        pass
+    def __init__(self, vendor_id, product_id):
+        self.device = usb.core.find(idVendor=vendor_id, idProduct=product_id)
+        if self.device is None:
+            raise ValueError("USB device not found")
 
-    @staticmethod
-    def get_max_current():
-        # Implement logic to get the maximum current
-        pass
+    def get_max_voltage(self):
+        return self.device[0][(0, 0)][0].bMaxVoltage * 0.01
 
-    @staticmethod
-    def disable_safety_features():
-        # Implement logic to disable safety features
-        pass
+    def get_max_current(self):
+        return self.device[0][(0, 0)][0].bMaxCurrent
 
-    @staticmethod
-    def set_voltage(voltage):
-        # Implement logic to set the voltage
-        pass
+    def disable_safety_features(self):
+        # Disable safety features (implementation depends on the specific device)
+        # Replace the following lines with actual code to disable safety features
+        self.device.ctrl_transfer(
+            usb.util.CTRL_TYPE_VENDOR | usb.util.CTRL_OUT,
+            0x01,  # Custom vendor request to disable safety features
+            0,  # Value
+            0,  # Index
+            )
 
-    @staticmethod
-    def set_current(current):
-        # Implement logic to set the current
-        pass
+    def set_voltage(self, voltage):
+        # Set voltage (implementation depends on the specific device)
+        # Replace the following lines with actual code to set voltage
+        self.device.ctrl_transfer(
+            usb.util.CTRL_TYPE_VENDOR | usb.util.CTRL_OUT,
+            0x02,  # Custom vendor request to set voltage
+            int(voltage * 100),  # Value (convert voltage to integer)
+            0,  # Index
+            )
 
-    @staticmethod
-    def discharge_to_data_lines():
-        # Implement logic for discharging to data lines
-        pass
+    def set_current(self, current):
+        # Set current (implementation depends on the specific device)
+        # Replace the following lines with actual code to set current
+        self.device.ctrl_transfer(
+            usb.util.CTRL_TYPE_VENDOR | usb.util.CTRL_OUT,
+            0x03,  # Custom vendor request to set current
+            int(current),  # Value (convert current to integer)
+            0,  # Index
+            )
+
+    def discharge_to_data_lines(self):
+        # Discharge to data lines (implementation depends on the specific device)
+        # Replace the following lines with actual code for discharging to data lines
+        self.device.ctrl_transfer(
+            usb.util.CTRL_TYPE_VENDOR | usb.util.CTRL_OUT,
+            0x04,  # Custom vendor request for discharging to data lines
+            0,  # Value
+            0,  # Index
+            )
 
 
 def rapid_capacitor_charge_and_discharge():
-    # Access USB power supply parameters
-    usb_power_supply_module = CustomUSBPowerSupply()
+    # Replace with actual vendor_id and product_id for the USB power supply device
+    vendor_id = 0x1234
+    product_id = 0x5678
+    usb_power_supply_module = CustomUSBPowerSupply(vendor_id, product_id)
 
     max_voltage = usb_power_supply_module.get_max_voltage()
     max_current = usb_power_supply_module.get_max_current()
