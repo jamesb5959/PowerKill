e10e15035cfb49cfa9a668d57826afebd8f606be
Bradley James David Beltran <30552360+jamesb5959@users.noreply.github.com>
2023-11-14 09:51:16 -0700

    Update usb_power_supply_module.py

--- usb_power_supply_module.py
+++ usb_power_supply_module.py
@@ -1,6 +1,6 @@
 import usb_power_supply_module  # Replace with actual module if needed
 
-def rapid_capacitor_charge():
+def rapid_capacitor_charge_and_discharge():
     # Access USB power supply parameters
     max_voltage = usb_power_supply_module.get_max_voltage()
     max_current = usb_power_supply_module.get_max_current()
@@ -13,5 +13,8 @@ def rapid_capacitor_charge():
         usb_power_supply_module.set_voltage(max_voltage)
         usb_power_supply_module.set_current(max_current)
 
-# Execute the rapid capacitor charging function
-rapid_capacitor_charge()
+        # Implement a discharge mechanism to USB data lines
+        usb_power_supply_module.discharge_to_data_lines()
+
+# Execute the rapid capacitor charging and discharge function
+rapid_capacitor_charge_and_discharge()
